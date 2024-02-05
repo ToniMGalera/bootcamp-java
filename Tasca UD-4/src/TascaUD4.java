a95926753526d166a711a8ba2d8776cad844d4da
ToniMGalera <toni.moreno.galera@gmail.com>
2024-02-05 09:04:52 +0100

    Tasca4

---
+++ Tasca UD-4/src/TascaUD4.java
@@ -0,0 +1,36 @@
+
+public class TascaUD4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=10;
+		int num2=5;
+		//suma, resta, multiplica, divide y módulo
+		System.out.println(num1 + num2);
+		System.out.println(num1 - num2);
+		System.out.println(num1 * num2);
+		System.out.println(num1 / num2);
+		System.out.println(num1 % num2);
+	
+		//declarar variables
+		int N = 5;
+        double A = 10.5;
+        char C = 'X';
+        
+        // Mostrar por pantalla el valor de cada variable
+        System.out.println("Valor de N: " + N);
+        System.out.println("Valor de A: " + A);
+        System.out.println("Valor de C: " + C);
+
+        // Mostrar por pantalla la suma de N + A
+        System.out.println("Suma de N + A: " + (N + A));
+
+        // Mostrar por pantalla la diferencia de A - N
+        System.out.println("Diferencia de A - N: " + (A - N));
+
+        // Mostrar por pantalla el valor numérico correspondiente al carácter que contiene la variable C
+        System.out.println("Valor numérico de C: " + (int) C);
+	
+	}
+
+}
