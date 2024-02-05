69898e5c44c0888fca4fc0d36ba1d60f5fb63481
ToniMGalera <toni.moreno.galera@gmail.com>
2024-02-05 10:05:45 +0100

    operador

---
+++ operador/src/operador/PruebaOperador.java
@@ -0,0 +1,14 @@
+package operador;
+
+public class PruebaOperador {
+
+	public static void main(String[] args) {
+		
+		int op=5;
+		System.out.println(op);
+		System.out.println(op++);
+		System.out.println(++op);
+
+	}
+
+}
